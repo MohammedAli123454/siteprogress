f0998e4f8f0a30e492a01677ea73dd16055ca7b8
MohammedAli123454 <azcsmohdali1@gmail.com>
2024-11-08 06:38:42 +0300

    Trade Comp

--- app/stepperForm/page.tsx
+++ app/stepperForm/page.tsx
@@ -247,7 +247,7 @@ import React, { useState } from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
-import tradeList from '@/app/tradelist.json';
+import tradeList from '@/app/tradeList.json';
 
 // Define the Trade interface
 interface Trade {
